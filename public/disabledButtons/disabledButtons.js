66534c80292c2180cbb1647740ad737257a63966
David M. Anderson <dma@epsilondelta.us>
2017-06-27 11:40:15 -0700

    Added disabledButtons test page

---
+++ public/disabledButtons/disabledButtons.js
@@ -0,0 +1,144 @@
+/*
+    disabledButtons.js
+
+    Code for testing some approaches to disabling buttons
+*/
+
+setupAttrForm( );
+setupAriaForm( );
+setupNonvisualForm( );
+
+//=============================================================================
+
+function setupAttrForm( ) {
+    var $input = $('#attrForm input');
+    var $button = $('#attrForm button');
+    var $output = $('#attrForm .output')
+
+    disableButton( $button );
+    $input.on( 'change input', handleInput );
+    $button.on( 'click', handleSubmit );
+
+    function handleInput( ) {
+        $output.empty( );
+        if ( $input.val().length ) {
+            enableButton( $button );
+        } else {
+            disableButton( $button );
+        }
+    }
+
+    function handleSubmit( ) {
+        if ( isEnabled( $button) ) {
+            $output.text( 'You entered "' + $input.val() + '"');
+        }
+    }
+
+    function enableButton( btn ) {
+        $(btn).prop( 'disabled', false );
+    }
+
+    function disableButton( btn ) {
+        $(btn).prop( 'disabled', true );
+    }
+
+    function isEnabled( btn ) {
+        return $(btn).prop( 'disabled' ) === false;
+    }
+}
+
+//=============================================================================
+
+function setupAriaForm( ) {
+    var $input = $('#ariaForm input');
+    var $button = $('#ariaForm button');
+    var $output = $('#ariaForm .output')
+
+    disableButton( $button );
+    $input.on( 'change input', handleInput );
+    $button.on( 'click', handleSubmit );
+
+    function handleInput( ) {
+        $output.empty( );
+        if ( $input.val().length ) {
+            enableButton( $button );
+        } else {
+            disableButton( $button );
+        }
+    }
+
+    function handleSubmit( ) {
+        if ( isEnabled( $button) ) {
+            $output.text( 'You entered "' + $input.val() + '"');
+        }
+    }
+
+    function enableButton( btn ) {
+        $(btn).attr( 'aria-disabled', 'false' );
+        var origTabindex = $(btn).attr( 'data-orig-tabindex' ) || 0;
+        $(btn).attr( 'tabindex', origTabindex );
+    }
+
+    function disableButton( btn ) {
+        $(btn).attr( 'aria-disabled', 'true' );
+        var origTabindex = $(btn).attr( 'tabindex' ) || 0;
+        $(btn).attr( 'data-orig-tabindex', origTabindex );
+        $(btn).attr( 'tabindex', -1 );
+    }
+
+    function isEnabled( btn ) {
+        return $(btn).attr( 'aria-disabled' ) !== 'true';
+    }
+}
+
+//=============================================================================
+
+
+function setupNonvisualForm( ) {
+    var $input = $('#nonvisualForm input');
+    var $button = $('#nonvisualForm button');
+    var $output = $('#nonvisualForm .output')
+
+    disableButton( $button );
+    $input.on( 'change input', handleInput );
+    $button.on( 'click', handleSubmit );
+
+    function handleInput( ) {
+        $output.empty( );
+        if ( $input.val().length ) {
+            enableButton( $button );
+        } else {
+            disableButton( $button );
+        }
+    }
+
+    function handleSubmit( ) {
+        if ( isEnabled( $button) ) {
+            $output.text( 'You entered "' + $input.val() + '"');
+        }
+    }
+
+    function enableButton( btn ) {
+        $(btn).removeClass( 'disabled' );
+        $(btn).find( '.nonvisual' ).remove( );
+        var origTabindex = $(btn).attr( 'data-orig-tabindex' ) || 0;
+        $(btn).attr( 'tabindex', origTabindex );
+    }
+
+    function disableButton( btn ) {
+        $(btn).addClass( 'disabled' );
+        var $nonvis = $('<span>')
+            .addClass( 'nonvisual' )
+            .text( 'disabled' );
+        $(btn).append( $nonvis );
+        var origTabindex = $(btn).attr( 'tabindex' ) || 0;
+        $(btn).attr( 'data-orig-tabindex', origTabindex );
+        $(btn).attr( 'tabindex', -1 );
+    }
+
+    function isEnabled( btn ) {
+        return $(btn).hasClass( 'disabled' ) === false;
+    }
+}
+
+//=============================================================================
